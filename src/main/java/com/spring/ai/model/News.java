e9396b0681c74e0a4c3d9725237df151bcddc529
Siddu4101 <siddu412001@gmail.com>
2025-09-21 00:47:47 +0530

    getting structured response and different ways of getting response and also tweaking the model properties

---
+++ src/main/java/com/spring/ai/model/News.java
@@ -0,0 +1,13 @@
+package com.spring.ai.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class News {
+    String topic;
+    String shortExplanation;
+}
--- src/main/java/com/spring/ai/rest/SpringAiController.java
+++ src/main/java/com/spring/ai/rest/SpringAiController.java
@@ -1,12 +1,20 @@
 package com.spring.ai.rest;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.metadata.ChatResponseMetadata;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.ai.model.News;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/spring-ai")
 class SpringAiController {
@@ -20,6 +28,61 @@ class SpringAiController {
 
     @GetMapping("/chat")
     public ResponseEntity<String> askQuestion(@RequestParam String q){
+
+//        /*passing prompt instead of simple query*/
+        Prompt prompt = new Prompt("What can you do for me ?");
+        String content = chatClient.prompt(prompt).call().content();
+
+        log.info("Response from using prompt obj {}", content);
+
+        /*getting metadata about the request*/
+        ChatResponseMetadata metadata = chatClient.prompt(q)
+                .call()
+                .chatResponse()
+                .getMetadata();
+        log.info("MetaDate: {} ", metadata);
+
+        /*chatResponse*/
+        String response = chatClient.prompt(q)
+                .call()
+                .chatResponse()
+                .getResults() /*multi response*/
+                .get(0)/*picking one*/
+                .getOutput()
+                .getText();
+
+        log.info("The response from the model {}", response);
+
+        /*using simple string*/
        return ResponseEntity.ok(chatClient.prompt(q).call().content());
+
+    }
+
+    @GetMapping("/news")
+    public ResponseEntity<News> getNewNews(){
+
+        /*setting default chat options this can be done at any level for chat client(while building chat client too) */
+        /*at prompt level */
+        Prompt prompt = new Prompt("get a new topic and some short description about the topic you chosen and Respond ONLY with a single valid JSON object, not an array"
+                , OllamaOptions.builder().temperature(0.8).build());
+
+        News asNewContentCreator = chatClient.prompt(prompt)
+                .system("as new content creator")
+                .call()
+                .entity(News.class);/*to map the response to a structure*/
+
+        log.info("The new response with tweaked temp {}", asNewContentCreator);
+
+
+        /*returning a response in a structured way*/
+        return ResponseEntity.ok(
+                chatClient
+                        .prompt()
+                        .user("get a new topic and some short description about the topic you chosen and Respond ONLY with a single valid JSON object, not an array")
+                        .system("act as a senior journalist")/*to give more context and actor */
+                        .call()
+                        .entity(News.class)/*to map the response to a structure*/
+
+        );
     }
 }
