ebaef0c649d8e81d9c358153328495124b394a47
Siddu4101 <siddu412001@gmail.com>
2025-10-01 22:26:42 +0530

    moving the chatClient builder to config class

--- src/main/java/com/spring/ai/config/BasicBeanConfig.java
+++ src/main/java/com/spring/ai/config/BasicBeanConfig.java
@@ -1,14 +1,17 @@
 package com.spring.ai.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class BasicBeanConfig {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder){
+        log.info("Creating a bean for chat client");
         return chatClientBuilder.build();
     }
 }
